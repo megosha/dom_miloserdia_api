90f6bacfb1a14fe1df611c995f37b6455d547b9a
Margarita <mv@asofts.ru>
2020-04-09 16:25:57 +0700

    secret key added bd version 1 ready

--- dom_miloserdia_api/settings.py
+++ dom_miloserdia_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from .secret import SECRET_KEY as SKEY
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u&sx^zm9=bz3u+3=e3+v#293m33xs6@1y^*1bv-jq+mejaj!kh'
+SECRET_KEY = SKEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

1b5714151e43bbe0b4b8ca64aa8b7e33b53fd5cf
Margarita <mv@asofts.ru>
2020-04-28 14:08:13 +0700

    static root in urls

--- dom_miloserdia_api/urls.py
+++ dom_miloserdia_api/urls.py
@@ -19,7 +19,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from front.views import Logout
 
-
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('admin_domm/logout/', Logout.as_view()),
@@ -29,3 +28,4 @@ urlpatterns = [
 ]
 
 urlpatterns.extend(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
+urlpatterns.extend(static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
